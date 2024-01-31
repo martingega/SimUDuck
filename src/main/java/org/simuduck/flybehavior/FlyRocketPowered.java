59b57b9c0993c3e0c2d2534b74e6d4232b4f3994
martingega <martin.gega@studenti.unimi.it>
2024-01-31 14:56:49 +0100

    inserted a new rocket powered flying behavior

---
+++ src/main/java/org/simuduck/flybehavior/FlyRocketPowered.java
@@ -0,0 +1,9 @@
+package org.simuduck.flybehavior;
+
+public class FlyRocketPowered implements FlyBehavior {
+
+    @Override
+    public String fly() {
+        return "I am flying with a rocket!";
+    }
+}
