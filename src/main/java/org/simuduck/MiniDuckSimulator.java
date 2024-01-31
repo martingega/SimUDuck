daec905fdaaf5fbb61d1f256cb831d4cb128f4c7
martingega <martin.gega@studenti.unimi.it>
2024-01-31 15:02:55 +0100

    inserted ModelDuck in main to test the dynamicity

--- src/main/java/org/simuduck/MiniDuckSimulator.java
+++ src/main/java/org/simuduck/MiniDuckSimulator.java
@@ -2,6 +2,8 @@ package org.simuduck;
 
 import org.simuduck.ducks.Duck;
 import org.simuduck.ducks.MallardDuck;
+import org.simuduck.ducks.ModelDuck;
+import org.simuduck.flybehavior.FlyRocketPowered;
 
 public class MiniDuckSimulator {
 
@@ -10,6 +12,14 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         System.out.println(mallard.performQuack()); // this calls the MallardDuck's inherited performQuack() method which calls quack() on the duck's inherited quackBehavior reference
         System.out.println(mallard.performFly()); // the same with MallardDuck's inherited performFly() method
+
+        Duck model = new ModelDuck();
+        // initially the model duck cannot fly
+        System.out.println(model.performFly()); // this delegates to the flyBehavior object set in the ModelDuck's conctructor, which is a FlyNoWay instance
+        // we change the model duck flying capability giving it a rocket poweder flying behavior
+        model.setFlyBehavior(new FlyRocketPowered()); // this invokes the model's inherited behavior setter method
+        // now the model duck dynamically changed its flying behavior
+        System.out.println(model.performFly());
     }
 
 }
